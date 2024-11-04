80a7252377b8b4b37fdcca2c18d304c65e50d811
RCT4H3 <139926077+RCT4H3@users.noreply.github.com>
2024-11-03 20:48:25 -0500

    Primer avance

---
+++ GTICS_LAB8_20210535/src/main/java/com/example/gtics_lab8_20210535/GticsLab820210535Application.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab8_20210535;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab820210535Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GticsLab820210535Application.class, args);
+    }
+
+}
---
+++ GTICS_LAB8_20210535/src/test/java/com/example/gtics_lab8_20210535/GticsLab820210535ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab8_20210535;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab820210535ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
